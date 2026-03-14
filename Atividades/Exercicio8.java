5bba8e2ca5ad2946e9ae06b937d126d3f937e7cd
sabrina <sabrinabrito0804@gmail.com>
2026-03-14 18:07:48 -0300

    Adiciona resolução do Exercício 8

---
+++ Atividades/Exercicio8.java
@@ -0,0 +1,19 @@
+// Nome: Sabrina Custódio Brito
+// Matrícula: 1252211621341
+
+import java.util.Scanner;
+public class Exercicio8 {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in); 
+        System.out.print("Digite a temperatura em Celsius:");
+        double a = entrada.nextDouble();
+        double resultadof = (a * 9.0/5) + 32;
+        double resultadok = a + 273.15;
+
+
+        System.out.println("Fahrenheit: " + resultadof + " Km/l");
+        System.out.println("Kelvin: " + resultadok + " Km/l");
+
+        entrada.close();
+    }
+}
